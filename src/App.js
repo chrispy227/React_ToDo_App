f453e016f1f34bc8728e32fde9176395d5f610bd
chrispy227 <chbppp@gmail.com>
2020-09-10 05:07:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// Importing Components
+import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputText, setInputText] = useState("");
+	return (
+		<div className="App">
+			<header>
+				<h1>Chris's ToDo List {inputText}</h1>
+			</header>
+			<Form setInputText={setInputText} />
+			<ToDoList />
+		</div>
+	);
 }
 
 export default App;
